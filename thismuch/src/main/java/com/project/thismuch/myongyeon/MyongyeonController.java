a310f6f4988705759942cb35de14acb25c907811
auddus16 <auddus16@naver.com>
2022-05-04 23:04:51 +0900

    RestTemplateService-오픈뱅킹 통신

--- thismuch/src/main/java/com/project/thismuch/myongyeon/MyongyeonController.java
+++ thismuch/src/main/java/com/project/thismuch/myongyeon/MyongyeonController.java
@@ -1,10 +1,13 @@
 package com.project.thismuch.myongyeon;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.project.thismuch.service.RestTemplateService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,10 +18,20 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping(value = "/my")
 public class MyongyeonController {
 	
+	@Autowired
+	private final RestTemplateService templateService;
+	
 	@GetMapping("/test")
 	public ResponseEntity<?> hi() {
 		log.info("test연결 완료");
 		return ResponseEntity.ok("TEST값");
+	}
+	
+	@GetMapping("/searchTran")
+	public ResponseEntity<?> searchTran(){
+		log.info("/my/searchTran 호출");
+		
+		return ResponseEntity.ok(templateService.searchTran());
 	}
 	
 //	@GetMapping("/auth/openbank/callback")
