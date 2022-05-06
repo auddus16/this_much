afe93291af09b55c7a4c6d415ca45ff35a0c095c
comeeasy <zz1236zz@naver.com>
2022-05-07 00:20:13 +0900

    feat: grant implemented

--- thismuch/src/main/java/com/project/thismuch/oauth/OAuthRepository.java
+++ thismuch/src/main/java/com/project/thismuch/oauth/OAuthRepository.java
@@ -1,2 +1,9 @@
-package com.project.thismuch.oauth;public interface OAuthRepository {
+package com.project.thismuch.oauth;
+
+import com.project.thismuch.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OAuthRepository extends CrudRepository<User, Long> {
 }
