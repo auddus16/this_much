4e6c237e7e6bd6690a28436f95ced0ee9f5e104e
auddus16 <auddus16@naver.com>
2022-05-09 22:53:05 +0900

    jpa설정 추가

--- thismuch/src/main/java/com/project/thismuch/ThismuchApplication.java
+++ thismuch/src/main/java/com/project/thismuch/ThismuchApplication.java
@@ -2,11 +2,17 @@ package com.project.thismuch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
+@EnableJpaRepositories(basePackages ={"com.project.thismuch.myongyeon.*"})
+@EntityScan(basePackages ={"com.project.thismuch.models.*"})
+@ComponentScan(basePackages= {"com.project.thismuch.moreInfo.*"})
 public class ThismuchApplication {
 
 	public static void main(String[] args) {
--- thismuch/src/main/java/com/project/thismuch/myongyeon/ThismuchController.java
+++ thismuch/src/main/java/com/project/thismuch/myongyeon/ThismuchController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
