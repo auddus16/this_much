57c4f757bcf17464debcfbe41dde370baa6caa95
auddus16 <auddus16@naver.com>
2022-05-14 11:40:29 +0900

    account builder

--- thismuch/src/main/java/com/project/thismuch/data/entities/AccountEntity.java
+++ thismuch/src/main/java/com/project/thismuch/data/entities/AccountEntity.java
@@ -1,57 +1,48 @@
 package com.project.thismuch.data.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.ForeignKey;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import com.project.thismuch.data.dto.AccountDTO;
+import com.project.thismuch.data.dto.CategoryDTO;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
 
 @Table(name = "account")
 @Entity(name = "Account")
 @Slf4j
 @Getter
 @Setter
-@RequiredArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class AccountEntity {
-    
-	@Id
+    @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "account_no")
-    @JsonIgnore
     private Integer accountNo;      // pk
-    
     @ManyToOne
     @JoinColumn(name = "user_no", foreignKey = @ForeignKey(name = "user_no"))
     private UserEntity userNo;         // fk
-    
     @Column(name = "bank_name")
     private String bankName;        // 은행이름
-    
     @Column(name = "account_number")
     private String accountNumber;   // 계좌번호
-    
     @Column(name = "account_name")
     private String accountName;     // 계좌별칭
-    
     @Column(name = "bank_code_std")
     private String bankCodeStd;     // 은행고유번호(국민:004)
-    
     @Column(name = "fintech_use_num")
     private String fintechUseNum;  // 핀테크이용번호
 
+    public AccountDTO toDTO() {
+        return AccountDTO.builder()
+                .accountNo(accountNo)
+                .userNo(userNo)
+                .bankName(bankName)
+                .accountNumber(accountNumber)
+                .accountName(accountName)
+                .bankCodeStd(bankCodeStd)
+                .fintechUseNum(fintechUseNum)
+                .build();
+    }
 }
